58353214ee88ab43d8c7d5b6f4a5f6933bb14d12
Bruce Soto <b.a.s.c.6226@hotmail.com>
2023-08-09 01:25:56 -0500

    Agregar combo boxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EscogerRutaComponent } from './components/escoger-ruta/escoger-ruta.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CrearVueloComponent } from './components/crear-vuelo/crear-vuelo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EscogerRutaComponent,
+    CrearVueloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/escoger-ruta/escoger-ruta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-escoger-ruta',
+  templateUrl: './escoger-ruta.component.html',
+  styleUrls: ['./escoger-ruta.component.css']
+})
+export class EscogerRutaComponent {
+
+}
