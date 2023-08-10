21ebb103f6511f7eb540ccc69890eefa5b426045
Bruce Soto <b.a.s.c.6226@hotmail.com>
2023-08-10 01:05:01 -0500

    Completar front para la ruta

--- src/app/components/escoger-ruta/escoger-ruta.component.ts
+++ src/app/components/escoger-ruta/escoger-ruta.component.ts
@@ -11,19 +11,35 @@ export class EscogerRutaComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     const searchInput = this.el.nativeElement.querySelector('.search input[type="text"]');
-
     this.renderer.listen(searchInput, 'focus', () => {
       if (searchInput.value === "Buscar") {
         searchInput.value = "";
       }
     });
-
     this.renderer.listen(searchInput, 'blur', () => {
       if (searchInput.value.trim() === "") {
         searchInput.value = "Buscar";
       } else {
         searchInput.value = "Buscar";
       }
+    });
+
+    const checkboxes = this.el.nativeElement.querySelectorAll('.checkbox-input');
+    const fechaRegreso = this.el.nativeElement.querySelector('.fecha-regreso');
+    checkboxes.forEach((checkbox: HTMLElement) => {
+      this.renderer.listen(checkbox, 'change', () => {
+        checkboxes.forEach((otherCheckbox: HTMLElement) => {
+          if (otherCheckbox !== checkbox) {
+            (otherCheckbox as HTMLInputElement).checked = false;
+          } else {
+            if (checkbox.id === 'ida') {
+              fechaRegreso.disabled = true;
+            } else {
+              fechaRegreso.disabled = false;
+            }
+          }
+        });
+      });
     });
   }
 }
